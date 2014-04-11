b68c57d948fc76de516ebdc407d47cf2791678fa
Brendan Hubble <brendanhubble@gmail.com>
2014-04-11 15:53:55 +1000

    Should all work now

--- fetchToPrint.sh
+++ fetchToPrint.sh
@@ -7,19 +7,19 @@ export DISPLAY=:0.0
 source fetchToPrint.conf
 
 # Checking enviroment
-if [ ! -d "$FETCHTOPRINT/logs" ]
+if [ ! -d "$FETCHTOPRINT_DIR/logs" ]
 then
-	mkdir $FETCHTOPRINT/logs
+	mkdir $FETCHTOPRINT_DIR/logs
 fi
 
-if [ ! -d "$FETCHTOPRINT/mail" ]
+if [ ! -d "$FETCHTOPRINT_DIR/mail" ]
 then
-	mkdir $FETCHTOPRINT/mail
+	mkdir $FETCHTOPRINT_DIR/mail
 fi
 
-if [ ! -d "$FETCHTOPRINT/pdf-in" ]
+if [ ! -d "$FETCHTOPRINT_DIR/attachments" ]
 then
-	mkdir $FETCHTOPRINT/pdf-in
+	mkdir $FETCHTOPRINT_DIR/attachments
 fi
 
 # Creating fetchmail and procmail confs
@@ -27,12 +27,12 @@ sed -e "s/:MAIL_SERVER:/$MAIL_SERVER/g" \
     -e "s/:MAIL_PROTO:/$MAIL_PROTO/g" \
     -e "s/:MAIL_USER:/$MAIL_USER/g" \
     -e "s/:MAIL_PASSWORD:/$MAIL_PASSWORD/g" \
-    -e "s/:PROCMAIL_CONFIG:/$PROCMAIL_CONFIG/g" \
-    $FETCHTOPRINT/configs/fetchmail.template > $FETCHTOPRINT/configs/fetchmail
+    -e "s@:PROCMAIL_CONFIG:@$PROCMAIL_CONFIG@g" \
+    $FETCHTOPRINT_DIR/configs/fetchmail.template > $FETCHTOPRINT_DIR/configs/fetchmail
 
-sed -e "s/:FETCHTOPRINT_DIR:/$FETCHTOPRINT_DIR/g" \
-    -e "s/:PROCMAIL_LOG:/$PROCMAIL_LOG/g" \
-    $FETCHTOPRINT/configs/procmail.template > $FETCHTOPRINT/configs/procmail
+sed -e "s@:FETCHTOPRINT_DIR:@$FETCHTOPRINT_DIR@g" \
+    -e "s@:PROCMAIL_LOG:@$PROCMAIL_LOG@g" \
+    $FETCHTOPRINT_DIR/configs/procmail.template > $FETCHTOPRINT_DIR/configs/procmail
 
 # Operations
 fetchmail -K -s -f $FETCHMAIL_CONFIG 2>> $FETCHMAIL_LOG 1>> $FETCHMAIL_LOG # Grabs mail via IMAP
@@ -40,10 +40,10 @@ munpack $MAIL_NEWDIR -f -q -C $ATTACHMENT_DIR 2>> $MUNPACK_LOG 1>> $MUNPACK_LOG 
 
 files=$(ls $ATTACHMENT_DIR/*.$ATTACHMENT_TYPE 2> /dev/null | wc -l)	# counts number of pdfs in ATTACHMENT_DIR
 if [ "$files" != "0" ]; then # If the number of pdf's is greater then 0 then;
-	lp -d $CUPS_PRINTER -h $CUPS_HOSTNAME -o ColorModel=Grayscale $ATTACHMENT_DIR/*.$ATTACHMENT_TYPE >> $CUPS_LOG # Prints all $ATTACHEMNT_TYPE in $ATTACHMENT_DIR to VRH-PRN-002
-	echo $(date +'%Y-%m-%d %T') >> $PDF_PRINT_LOG # Prints out date and time a print job was done
-	find $ATTACHMENT_DIR/ -name *.$ATTACHMENT_TYPE | awk -F'/' '{ print "\t"$NF }' >> $PDF_PRINT_LOG # Prints only the PDF name to the log
+	lp -d $CUPS_PRINTER -h $CUPS_SERVER $ATTACHMENT_DIR/*.$ATTACHMENT_TYPE >> $CUPS_LOG # Prints all $ATTACHEMNT_TYPE in $ATTACHMENT_DIR to $CUPS_SERVER
+	echo $(date +'%Y-%m-%d %T') >> $FETCHTOPRINT_LOG # Prints out date and time a print job was done
+	find $ATTACHMENT_DIR/ -name *.$ATTACHMENT_TYPE | awk -F'/' '{ print "\t"$NF }' >> $FETCHTOPRINT_LOG # Prints only the $ATTACHMENT_TYPE name to the log
 	rm -r $ATTACHMENT_DIR/*.* > /dev/null
-	rm /opt/pdfPrint/mail/new/* > /dev/null
+	rm $FETCHTOPRINT_DIR/mail/new/* > /dev/null
 
 fi
