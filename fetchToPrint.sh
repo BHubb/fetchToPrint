bae304fc6f773c4368e9e77e55c030ca25988dd5
Brendan Hubble <brendanhubble@gmail.com>
2014-04-14 08:45:27 +1000

    Resolved it not printing and not reporting properly

--- fetchToPrint.sh
+++ fetchToPrint.sh
@@ -40,9 +40,10 @@ munpack $MAIL_NEWDIR -f -q -C $ATTACHMENT_DIR 2>> $MUNPACK_LOG 1>> $MUNPACK_LOG 
 
 files=$(ls $ATTACHMENT_DIR/*.$ATTACHMENT_TYPE 2> /dev/null | wc -l)	# counts number of pdfs in ATTACHMENT_DIR
 if [ "$files" != "0" ]; then # If the number of pdf's is greater then 0 then;
-	lp -d $CUPS_PRINTER -h $CUPS_SERVER $ATTACHMENT_DIR/*.$ATTACHMENT_TYPE >> $CUPS_LOG # Prints all $ATTACHEMNT_TYPE in $ATTACHMENT_DIR to $CUPS_SERVER
+	lp -d $CUPS_PRINTER -h $CUPS_SERVER:$CUPS_PORT $ATTACHMENT_DIR/*.$ATTACHMENT_TYPE 2>> $CUPS_LOG 1>> $CUPS_LOG # Prints all $ATTACHEMNT_TYPE in $ATTACHMENT_DIR to $CUPS_SERVER
 	echo $(date +'%Y-%m-%d %T') >> $FETCHTOPRINT_LOG # Prints out date and time a print job was done
-	find $ATTACHMENT_DIR/ -name *.$ATTACHMENT_TYPE | awk -F'/' '{ print "\t"$NF }' >> $FETCHTOPRINT_LOG # Prints only the $ATTACHMENT_TYPE name to the log
+	ls $ATTACHMENT_DIR/*.$ATTACHMENT_TYPE | awk -F'/' '{ print "\t"$NF }' >> $FETCHTOPRINT_LOG # Prints only the $ATTACHMENT_TYPE name to the log
+	# Clean up
 	rm -r $ATTACHMENT_DIR/*.* > /dev/null
 	rm $FETCHTOPRINT_DIR/mail/new/* > /dev/null
 
